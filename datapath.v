7b2c54f8594267398bff2b3226ac49c6c2114ad2
SeaWalks <80789948+SeaWalks@users.noreply.github.com>
2021-03-16 15:07:14 -0700

    Update datapath.v
    formatting

--- datapath.v
+++ datapath.v
@@ -17,6 +17,7 @@ module datapath(
     PCRegister pc(.clk(clk), .reset(reset), .Din(PCAddout), .PC_out(PCOut));
     PCADD pcadd(.Din(PCOut), .PC_add_out(PCAddout));
     Instruction_Memory im(.Addr(PCOut), .Inst_out(IMout));
-    regfile32 rg(.clk(clk), .reset(reset), .D_En(RegWrite), .D_Addr(IMout[15:11]), .S_Addr(IMout[25:21]), .T_Addr(rtIn), .D(writeData), .S(rs), .T(rt));    control cnt(.Op(IMout[5:0]), .Func(IMout[31:26]), .RegWrite(RegWrite), .ALUCntl(ALUCntl));
+    regfile32 rg(.clk(clk), .reset(reset), .D_En(RegWrite), .D_Addr(IMout[15:11]), .S_Addr(IMout[25:21]), .T_Addr(rtIn), .D(writeData), .S(rs), .T(rt));    
+    control cnt(.Op(IMout[5:0]), .Func(IMout[31:26]), .RegWrite(RegWrite), .ALUCntl(ALUCntl));
     alu V(.A(rs), .B(rt), .ALUCntrl(ALUCntrl), .ALU_Out(Dout), .N(Negative), .Z(Zero), .C(Carry), .V(Overflow));
 endmodule
