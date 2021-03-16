6a7739dc07cbbb93eddc1ad5077750230f70727d
SeaWalks <80789948+SeaWalks@users.noreply.github.com>
2021-03-16 14:13:10 -0700

    Update datapath.v
    Fixed connections to other modules

--- datapath.v
+++ datapath.v
@@ -9,7 +9,7 @@ module datapath(
     output [31:0] Dout
     );
     //Wire Declarations
-    wire [31:0]IMout, PCout, PCAddout, rs, rt;
+    wire [31:0]IMout, PCout, PCAddout, rs, rt, writeData;
     wire [3:0] ALUCntl; 
     wire Negative, Zero, Overflow, Carry, RegWrite;
     
@@ -17,7 +17,6 @@ module datapath(
     PCRegister pc(.clk(clk), .reset(reset), .Din(PCAddout), .PC_out(PCOut));
     PCADD pcadd(.Din(PCOut), .PC_add_out(PCAddout));
     Instruction_Memory im(.Addr(PCOut), .Inst_out(IMout));
-    regfile32 rf(.clk(clk), .reset(reset), .D_En(RegWrite), .D_Addr(IMout[11:15]), .S_Addr(IMout[25:21]), .T_Addr(IMout[20:16]), .D(Dout), .s(rs), .t(rt));
-    control cnt(.Op(IMout[5:0]), .Func(IMout[31:26]), .RegWrite(RegWrite), .ALUCntl(ALUCntl));
-    ALU V(.A(rs), .B(rt), .ALUCntl(ALUCntrl), .ALUout(Dout), .N(Negative), .Z(Zero), .C(Carry), .V(Overflow));
+    regfile32 rg(.clk(clk), .reset(reset), .D_En(RegWrite), .D_Addr(IMout[15:11]), .S_Addr(IMout[25:21]), .T_Addr(rtIn), .D(writeData), .S(rs), .T(rt));    control cnt(.Op(IMout[5:0]), .Func(IMout[31:26]), .RegWrite(RegWrite), .ALUCntl(ALUCntl));
+    alu V(.A(rs), .B(rt), .ALUCntrl(ALUCntrl), .ALU_Out(Dout), .N(Negative), .Z(Zero), .C(Carry), .V(Overflow));
 endmodule
