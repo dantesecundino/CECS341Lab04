53017873f03f2862605dc5e2f510da759f9537ad
SeaWalks <80789948+SeaWalks@users.noreply.github.com>
2021-03-16 16:25:51 -0700

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -1,23 +1,25 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-//Eric Chhour & Daniel Secundino
-//////////////////////////////////////////////////////////////////////////////////
-
-module datapath(
+module Datapath(
     input clk,
     input reset,
     output [31:0] Dout
     );
-    //Wire Declarations
-    wire [31:0]IMout, PCout, PCAddout, rs, rt, writeData;
-    wire [3:0] ALUCntl; 
-    wire Negative, Zero, Overflow, Carry, RegWrite;
+    wire [31:0] pcAddress, pcAdder, instructionMem, rsOut, rtOut, writeData;
+    wire [5:0] op, func;
+    wire [4:0] rsIn, rtIn, rdIn;
+    wire [3:0] ALUCntl;
+    wire RegWrite;
+
+    assign op = instructionMem[31:26];
+    assign rsIn = instructionMem[25:21];
+    assign rtIn = instructionMem[20:16];
+    assign rdIn = instructionMem[15:11];
+    assign func = instructionMem[5:0];
     
-    //Wire everything together
-    PCRegister pc(.clk(clk), .reset(reset), .Din(PCAddout), .PC_out(PCOut));
-    PCADD pcadd(.Din(PCOut), .PC_add_out(PCAddout));
-    Instruction_Memory im(.Addr(PCOut), .Inst_out(IMout));
-    regfile32 rg(.clk(clk), .reset(reset), .D_En(RegWrite), .D_Addr(IMout[15:11]), .S_Addr(IMout[25:21]), .T_Addr(rtIn), .D(writeData), .S(rs), .T(rt));    
-    control cnt(.Op(IMout[5:0]), .Func(IMout[31:26]), .RegWrite(RegWrite), .ALUCntl(ALUCntl));
-    alu V(.A(rs), .B(rt), .ALUCntrl(ALUCntrl), .ALU_Out(Dout), .N(Negative), .Z(Zero), .C(Carry), .V(Overflow));
+    PCRegister PC(.clk(clk), .reset(reset), .Din(pcAdder), .PC_out(pcAddress));
+    PCADD PCadd(.Din(pcAddress), .PC_add_out(pcAdder));
+    Instruction_Memory Imem(.Addr(pcAddress),.Inst_out(instructionMem));
+    control cnt(.Op(op), .Func(func), .RegWrite(RegWrite), .ALUCntl(ALUCntl));
+    regfile32 register(.clk(clk), .reset(reset), .D_En(RegWrite), .D_Addr(rdIn), .S_Addr(rsIn), .T_Addr(rtIn), .D(writeData), .S(rsOut), .T(rtOut));
+    alu V(.A(rsOut), .B(rtOut), .ALUCntrl(ALUCntl), .ALU_Out(Dout));
+    assign writeData = Dout;
 endmodule
